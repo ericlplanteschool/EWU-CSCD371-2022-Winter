8a39aa317ba9c1d0e10482e08a5f16f8c96425a9
Mike <imdukie@gmail.com>
2022-02-24 10:52:54 -0800

    Cleaned up unit tests.

--- Assignment/Assignment.Tests/SampleDataTests.cs
+++ Assignment/Assignment.Tests/SampleDataTests.cs
@@ -16,6 +16,7 @@ namespace Assignment.Tests
         [TestMethod]
         public void CsvRows_Read_Success()
         {
+            // Assert
             Assert.IsNotNull(sampleData.CsvRows);
             Assert.AreEqual<int>(50, sampleData.CsvRows.Count());
         }
@@ -26,39 +27,41 @@ namespace Assignment.Tests
             // Arrange
             IEnumerable<string> csvRows = sampleData.CsvRows;
 
-            // Act.
+            // Act
 
-            // Assert.
+            // Assert
             Assert.AreEqual<string>("1,Priscilla,Jenyns,pjenyns0@state.gov,7884 Corry Way,Helena,MT,70577", csvRows.First());
         }
 
         [TestMethod]
-        public void GetUniqueSortedListOfStatesGivenCsvRows_HardCoded()
+        public void GetUniqueSortedListOfStatesGivenCsvRows_SpokaneCompareToAllCities_WAOnlyListsIdentical()
         {
+            // Arrange
             List<IAddress> addresses = new();
             addresses.Add(new Address("53 Grim Point", "Spokane", "WA", "99022"));
             addresses.Add(new Address("1 Rutledge Point", "Spokane", "WA", "99021"));
             addresses.Add(new Address("6487 Pepper Wood Court", "Spokane", "WA", "99021"));
 
+            // Act
             IEnumerable<string> spokaneAddressStates = addresses.Select(address => address.State).Distinct();
             IEnumerable<string> states = sampleData.GetUniqueSortedListOfStatesGivenCsvRows().Select(state => "WA").Distinct();
-
             string statesInSpokane = string.Join(", ", spokaneAddressStates.ToArray());
             string allStatesWAOnly = string.Join(", ", states.ToArray());
 
+            // Assert
             Assert.AreEqual<string>(statesInSpokane, allStatesWAOnly);
         }
 
         [TestMethod]
-        public void GetUniqueSortedListOfStatesGivenCsvRows_isSorted()
+        public void GetUniqueSortedListOfStatesGivenCsvRows_Read_ListIsSortedAndUnique()
         {
-            // Arrange.
+            // Arrange
             List<string> list = sampleData.GetUniqueSortedListOfStatesGivenCsvRows().ToList();
             IEnumerable<string> temp = sampleData.CsvRows.Select(line => line.Split(',')).Select(column => column[6]).OrderBy(state => state).Distinct();
 
-            // Act.
+            // Act
 
-            // Assert.
+            // Assert
             Assert.IsTrue(list.SequenceEqual(temp));
         }
 
@@ -66,50 +69,47 @@ namespace Assignment.Tests
         [TestMethod]
         public void GetAggregateSortedListOfStatesUsingCsvRows_Read_Success()
         {
-            // Arrange.
+            // Arrange
             string states = sampleData.GetAggregateSortedListOfStatesUsingCsvRows();
 
-            // Assert.
+            // Assert
             Assert.AreEqual<string>("AL, AZ, CA, DC, FL, GA, IN, KS, LA, MD, MN, MO, MT, NC, NE, NH, NV, NY, OR, PA, SC, TN, TX, UT, VA, WA, WV", states);
         }
 
         [TestMethod]
         public void People_Read_Success()
         {
-            // Arrange.
+            // Arrange
             IEnumerable<string> csvRows = sampleData.CsvRows;
             IEnumerable<IPerson> people = sampleData.People;
 
-            // Assert.
+            // Assert
             Assert.AreEqual<int>(csvRows.Count(), people.Count());
         }
 
         [TestMethod]
-        public void FilterByEmail_filtersCorrectly()
+        public void FilterByEmailAddress_Filter_Success()
         {
-            // Arrange.
+            // Arrange
             Predicate<string> filter = email;
             static bool email(string email) => email.Contains("pjenyns0@state.gov");
             IEnumerable<(string, string)> result = sampleData.FilterByEmailAddress(filter);
 
-            // Act.
+            // Act
             var expected = ("Priscilla", "Jenyns");
 
-            // Assert.
+            // Assert
             Assert.AreEqual(expected, (result.First().Item1, result.First().Item2));
         }
 
         [TestMethod]
         public void GetAggregateListOfStatesGivenPeopleCollection_Read_Success()
         {
-            // Arrange.
+            // Arrange
             string uniqueRows = sampleData.GetAggregateSortedListOfStatesUsingCsvRows();
             string uniquePeople = sampleData.GetAggregateListOfStatesGivenPeopleCollection(sampleData.People);
 
-            // Act.
-
-
-            // Assert.
+            // Assert
             Assert.AreEqual(uniqueRows, uniquePeople);
         }
 
