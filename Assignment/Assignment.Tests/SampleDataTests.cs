fd5d9fa312fbcb39418196a4756bef7f4f1815d3
ericlplanteschool <elaplante@ewu.edu>
2022-02-24 21:03:48 -0800

    IE implementation round 2

---
+++ Assignment/Assignment.Tests/SampleDataTests.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Tests
+{
+    [TestClass]
+    public class SampleDataTests
+    {
+
+        readonly SampleData sampleData = new();
+
+        [TestMethod]
+        public void CsvRows_Read_Success()
+        {
+            // Assert
+            Assert.IsNotNull(sampleData.CsvRows);
+            Assert.AreEqual<int>(50, sampleData.CsvRows.Count());
+        }
+
+        [TestMethod]
+        public void CsvRows_Read_FirstRowSkipped()
+        {
+            // Arrange
+            IEnumerable<string> csvRows = sampleData.CsvRows;
+
+            // Act
+
+            // Assert
+            Assert.AreEqual<string>("1,Priscilla,Jenyns,pjenyns0@state.gov,7884 Corry Way,Helena,MT,70577", csvRows.First());
+        }
+
+        [TestMethod]
+        public void GetUniqueSortedListOfStatesGivenCsvRows_SpokaneCompareToAllCities_WAOnlyListsIdentical()
+        {
+            // Arrange
+            List<IAddress> addresses = new();
+            addresses.Add(new Address("53 Grim Point", "Spokane", "WA", "99022"));
+            addresses.Add(new Address("1 Rutledge Point", "Spokane", "WA", "99021"));
+            addresses.Add(new Address("6487 Pepper Wood Court", "Spokane", "WA", "99021"));
+
+            // Act
+            IEnumerable<string> spokaneAddressStates = addresses.Select(address => address.State).Distinct();
+            IEnumerable<string> states = sampleData.GetUniqueSortedListOfStatesGivenCsvRows().Select(state => "WA").Distinct();
+            string statesInSpokane = string.Join(", ", spokaneAddressStates.ToArray());
+            string allStatesWAOnly = string.Join(", ", states.ToArray());
+
+            // Assert
+            Assert.AreEqual<string>(statesInSpokane, allStatesWAOnly);
+        }
+
+        [TestMethod]
+        public void GetUniqueSortedListOfStatesGivenCsvRows_Read_ListIsSortedAndUnique()
+        {
+            // Arrange
+            List<string> list = sampleData.GetUniqueSortedListOfStatesGivenCsvRows().ToList();
+            IEnumerable<string> temp = sampleData.CsvRows.Select(line => line.Split(',')).Select(column => column[6]).OrderBy(state => state).Distinct();
+
+            // Act
+
+            // Assert
+            Assert.IsTrue(list.SequenceEqual(temp));
+        }
+
+
+        [TestMethod]
+        public void GetAggregateSortedListOfStatesUsingCsvRows_Read_Success()
+        {
+            // Arrange
+            string states = sampleData.GetAggregateSortedListOfStatesUsingCsvRows();
+
+            // Assert
+            Assert.AreEqual<string>("AL, AZ, CA, DC, FL, GA, IN, KS, LA, MD, MN, MO, MT, NC, NE, NH, NV, NY, OR, PA, SC, TN, TX, UT, VA, WA, WV", states);
+        }
+
+        [TestMethod]
+        public void People_Read_Success()
+        {
+            // Arrange
+            IEnumerable<string> csvRows = sampleData.CsvRows;
+            IEnumerable<IPerson> people = sampleData.People;
+
+            // Assert
+            Assert.AreEqual<int>(csvRows.Count(), people.Count());
+        }
+
+        [TestMethod]
+        public void FilterByEmailAddress_Filter_Success()
+        {
+            // Arrange
+            Predicate<string> filter = email;
+            static bool email(string email) => email.Contains("pjenyns0@state.gov");
+            IEnumerable<(string, string)> result = sampleData.FilterByEmailAddress(filter);
+
+            // Act
+            var expected = ("Priscilla", "Jenyns");
+
+            // Assert
+            Assert.AreEqual(expected, (result.First().Item1, result.First().Item2));
+        }
+
+        [TestMethod]
+        public void GetAggregateListOfStatesGivenPeopleCollection_Read_Success()
+        {
+            // Arrange
+            string uniqueRows = sampleData.GetAggregateSortedListOfStatesUsingCsvRows();
+            string uniquePeople = sampleData.GetAggregateListOfStatesGivenPeopleCollection(sampleData.People);
+
+            // Assert
+            Assert.AreEqual(uniqueRows, uniquePeople);
+        }
+
+    }
+}
---
+++ Assignment/Assignment/Node.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+
+namespace GenericsHomework
+{
+    public class Node<T> : IEnumerable<Node<T>>
+    {
+        public T Value { get; private set; }
+        public Node<T> Next { get; private set; }
+
+        public Node(T value)
+        {
+            Value = value;
+            Next = this;
+        }
+
+        public override string? ToString()
+        {
+            return Value?.ToString();
+        }
+
+        public void Append(T value)
+        {
+            if (Exists(value))
+            {
+                throw new ArgumentException("Existing value.");
+            }
+            else
+            {
+                Node<T> newNode = new(value);
+                newNode.Next = Next;
+                Next = newNode;
+            }
+        }
+
+        public void Clear()
+        {
+            Next = this;
+        }
+
+        public bool Exists(T value)
+        {
+            Node<T> current = this;
+            do
+            {
+                if (current.Value is not null)
+                {
+                    if (current.Value.Equals(value))
+                    {
+                        return true;
+                    }
+                }
+                current = current.Next;
+            } while (current != this);
+            return false;
+        }
+
+        public IEnumerator<Node<T>> GetEnumerator()
+        {
+            Node<T> cur = this;
+
+            do
+            {
+                yield return cur;
+                cur = cur.Next;
+            } 
+            while (cur != this);
+            
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public IEnumerable<T> ChildItems(int max)
+        {
+            Node<T> cur = this;
+            int num = 0;
+
+            do
+            {
+                yield return cur;
+                cur = cur.Next;
+                num++;
+            }
+
+            while(num < max);
+        }
+    }
+}
